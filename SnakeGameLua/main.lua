ef1730327e8737eb713079bac769e898f8774bc0
William Alves <williamalvs45@gmail.com>
2025-04-27 13:44:15 -0300

    Etapa semifinal de main.lua

--- SnakeGameLua/main.lua
+++ SnakeGameLua/main.lua
@@ -1,13 +1,16 @@
 snake = {
-  x = 40,
-  y = 40,
-  speed = 20,
+  positions = {
+    { x = 100, y = 40 },
+    { x = 80,  y = 40 },
+    { x = 60,  y = 40 }
+  },
+  size = 20,
   width = 20,
   height = 20
 }
 
-ranX = math.floor(780 / 20)
-ranY = math.floor(580 / 20)
+ranX = math.floor(760 / 20)
+ranY = math.floor(560 / 20)
 
 fruit = {
   randomX = math.random(0, ranX) * 20,
@@ -16,7 +19,10 @@ fruit = {
   height = 20
 }
 
+score = 0
+
 direction = " "
+movDirection = " "
 
 function love.load()
   love.window.setTitle("Snake Game")
@@ -32,88 +38,93 @@ end
 
 isColision = false
 
+
+
 function love.update(dt)
+  head = { x = snake.positions[1].x, y = snake.positions[1].y }
+
   -- Verificação pra ficar parado
   if isGameOver then
 
   else
-    if snake.x >= 780 or snake.x <= 0 or snake.y <= 0 or snake.y >= 580 then
+    if snake.positions[1].x >= 780 or snake.positions[1].x <= 0 or snake.positions[1].y <= 0 or snake.positions[1].y >= 580 then
       gameOver()
     else
-      if love.keyboard.isDown("right") then
+      if love.keyboard.isDown("right") and direction ~= "left" then
         direction = "right"
-      elseif love.keyboard.isDown("left") then
+      elseif love.keyboard.isDown("left") and direction ~= "right" then
         direction = "left"
-      elseif love.keyboard.isDown("up") then
+      elseif love.keyboard.isDown("up") and direction ~= "down" then
         direction = "up"
-      elseif love.keyboard.isDown("down") then
+      elseif love.keyboard.isDown("down") and direction ~= "up" then
         direction = "down"
       end
 
       if direction == "right" then
-        snake.x = snake.x + snake.speed
+        head.x = head.x + snake.size
         love.timer.sleep(0.1)
       elseif direction == "left" then
-        snake.x = snake.x - snake.speed
+        head.x = head.x - snake.size
         love.timer.sleep(0.1)
       elseif direction == "up" then
-        snake.y = snake.y - snake.speed
+        head.y = head.y - snake.size
         love.timer.sleep(0.1)
       elseif direction == "down" then
-        snake.y = snake.y + snake.speed
+        head.y = head.y + snake.size
         love.timer.sleep(0.1)
       end
     end
   end
 
+  --- Colisão com a própria cobra
+
+  for i = 4, #snake.positions do
+    if head.x == snake.positions[i].x and head.y == snake.positions[i].y then
+      gameOver()
+    end
+  end
+
+
+  table.insert(snake.positions, 1, head)
 
   -- Colisão com a fruta
 
-  if snake.x == fruit.randomX and snake.y == fruit.randomY then
+  if head.x == fruit.randomX and head.y == fruit.randomY then
     isColision = true
+    score = score + 1
+  else
+    table.remove(snake.positions)
   end
-
-
-  --[[  if snake.x < fruit.width + fruit.randomX and
-      snake.x + fruit.width > fruit.randomX and
-      snake.y < fruit.height + fruit.randomY and
-      snake.y + fruit.height > fruit.randomY then
-    isColision = true
-  end
-
-  --]]
 
   if isColision then
     fruit.randomX = math.random(0, ranX) * 20
     fruit.randomY = math.random(0, ranY) * 20
     isColision = false
   end
-
-  function love.draw()
-    -- Snake config
-    love.graphics.setColor(43 / 255, 172 / 255, 43 / 255)
-    love.graphics.rectangle("fill", snake.x, snake.y, snake.width, snake.height)
-    love.graphics.setFont(fontePadrao)
-    love.graphics.print(snake.y)
-    love.graphics.print(snake.x, 0, 20)
-    love.graphics.print(fruit.randomX, 0, 40)
-    love.graphics.print(fruit.randomY, 0, 60)
-
-    -- Fruit config
-    love.graphics.setColor(255 / 255, 99 / 255, 71 / 255)
-    love.graphics.rectangle("fill", fruit.randomX, fruit.randomY, fruit.width, fruit.height)
-
-
-    -- Game over config
-    if isGameOver then
-      love.graphics.setColor(255 / 255, 99 / 255, 71 / 255)
-      love.graphics.setFont(fonte)
-      fontWidth = fonte:getWidth("GAME OVER!")
-      fontHeight = fonte:getHeight()
-      love.graphics.print("GAME OVER!", (love.graphics.getWidth() - fontWidth) / 2,
-        (love.graphics.getHeight() - fontHeight) / 2)
-    end
-  end
 end
 
--- Pesquisar todos os temas da atividade de historia individualmente, um resumo de forma bem resumida. Escolher apenas uma para apresentar. O individual é somente texto. Entregar o link do docs na atividade. Pode ser um vídeo didático (como o do trabalho de química)
+function love.draw()
+  -- Snake config
+  love.graphics.setColor(43 / 255, 172 / 255, 43 / 255)
+  love.graphics.rectangle("fill", head.x, head.y, snake.width, snake.height)
+  for i = 2, #snake.positions do
+    love.graphics.rectangle("fill", snake.positions[i].x, snake.positions[i].y, snake.width, snake.height)
+  end
+  love.graphics.setFont(fontePadrao)
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.print(score)
+  -- Fruit config
+  love.graphics.setColor(255 / 255, 99 / 255, 71 / 255)
+  love.graphics.rectangle("fill", fruit.randomX, fruit.randomY, fruit.width, fruit.height)
+
+
+  -- Game over config
+  if isGameOver then
+    love.graphics.setColor(255 / 255, 99 / 255, 71 / 255)
+    love.graphics.setFont(fonte)
+    fontWidth = fonte:getWidth("GAME OVER!")
+    fontHeight = fonte:getHeight()
+    love.graphics.print("GAME OVER!", (love.graphics.getWidth() - fontWidth) / 2,
+      (love.graphics.getHeight() - fontHeight) / 2)
+  end
+end
